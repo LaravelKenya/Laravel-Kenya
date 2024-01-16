7fc96a512f2c3d7c7e079d2f1d90d0fbde15dbfa
rono516 <ronocollins2000@gmail.com>
2024-01-16 09:12:02 +0300

    fix/frontend-content

--- resources/js/components/Home/Participants.vue
+++ resources/js/components/Home/Participants.vue
@@ -44,7 +44,7 @@ const images = [
                 :key="index"
             >
                 <div
-                    class="relative flex flex-col shadow-md rounded-xl overflow-hidden hover:shadow-lg hover:-translate-y-1 transition-all duration-300 max-w-sm"
+                    class="relative mt-3 flex flex-col shadow-md rounded-xl overflow-hidden hover:shadow-lg hover:-translate-y-1 transition-all duration-300 max-w-sm"
                 >
                     <a href="" class="z-20 absolute h-full w-full top-0 left-0"
                         >&nbsp;</a
